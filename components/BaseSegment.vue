d44cff022508e04dde79f577621c5ce18e338ec1
kysnrm <kysn.rm@gmail.com>
2020-04-04 16:32:37 +0900

    BaseSegment コンポーネントを作成

---
+++ components/BaseSegment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ speaker }} {{ script }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    speaker: {
+      type: String,
+      required: true
+    },
+    script: {
+      type: String,
+      required: true
+    },
+    startTime: {
+      type: Number,
+      required: true
+    }
+  }
+})
+</script>
