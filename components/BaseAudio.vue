8d99310f82e32247f23d93fc9fa4583cd57caa04
kysnrm <kysn.rm@gmail.com>
2020-04-04 13:19:52 +0900

    音声データをロード可能に

---
+++ components/BaseAudio.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <audio controls src="../assets/test.mp3"></audio>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => {}
+})
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,14 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      })
+    }
   }
 }
