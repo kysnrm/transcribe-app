98f91639799640b6973ba5692aa83ba310ff1102
kysnrm <kysn.rm@gmail.com>
2020-04-04 12:57:42 +0900

    セグメントごとに表示

--- components/BaseScript.vue
+++ components/BaseScript.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>{{ response.results.transcripts[0].transcript }}</div>
+  <div>
+    <div v-for="(item, index) in response.results.segments" :key="index">
+      {{ response.results.speaker_labels.segments[index].speaker_label }}
+      {{ item.alternatives[0].transcript }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
