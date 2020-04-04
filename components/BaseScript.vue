855ff3092819749529b41a3b9642ad162f9debe5
kysnrm <kysn.rm@gmail.com>
2020-04-04 12:49:21 +0900

    json をインポートして表示

---
+++ components/BaseScript.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ response.results.transcripts[0].transcript }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Response from '@/assets/asrOutput.json'
+
+export default Vue.extend({
+  data: () => {
+    return {
+      response: Response
+    }
+  }
+})
+</script>
