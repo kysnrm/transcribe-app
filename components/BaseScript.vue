5e3f57de40619d13e6f15cef4bb86b1fd5bef033
kysnrm <kysn.rm@gmail.com>
2020-04-06 16:55:25 +0900

    mount 時に DB と Vuex にデータを格納

--- components/BaseScript.vue
+++ components/BaseScript.vue
@@ -11,21 +11,28 @@
       @set-time="setTime"
       @update-script="updateScript($event, index)"
     />
+    <button @click="saveScript">保存する</button>
+    <!-- <button @click="resetScript">最初の状態に戻す</button> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop, Emit } from 'vue-property-decorator'
+
+import { DataStore } from '@aws-amplify/datastore'
+import { Segment } from '@/src/models'
+
 import { segmentStore } from '~/store'
 
 import Response from '@/assets/asrOutput.json'
 import BaseSegment from '@/components/BaseSegment.vue'
 
-type Segment = {
+type SegmentType = {
   speaker: string
   startTime: number
   endTime: number
   script: string
+  id?: string
 }
 
 @Component({
@@ -35,15 +42,33 @@ type Segment = {
 })
 export default class BaseScript extends Vue {
   response: object = Response
-  mounted() {
+  storedata: Segment[] = []
+
+  async mounted() {
+    this.storedata = await DataStore.query(Segment)
     segmentStore.refleshScript()
     const segments = Response.results.segments
-    for (let i = 0; i < segments.length; i++) {
-      const segment: Segment = {
-        speaker: Response.results.speaker_labels.segments[i].speaker_label,
-        startTime: Number(segments[i].start_time),
-        endTime: Number(segments[i].end_time),
-        script: segments[i].alternatives[0].transcript
+    if (this.storedata.length === 0) {
+      for (let i = 0; i < segments.length; i++) {
+        await DataStore.save(
+          new Segment({
+            speaker: Response.results.speaker_labels.segments[i].speaker_label,
+            startTime: Number(segments[i].start_time),
+            endTime: Number(segments[i].end_time),
+            script: segments[i].alternatives[0].transcript
+          })
+        )
+      }
+      this.storedata = await DataStore.query(Segment)
+    }
+    for (let i = 0; i < this.storedata.length; i++) {
+      const data = this.storedata[i]
+      const segment: SegmentType = {
+        speaker: data.speaker,
+        startTime: data.startTime,
+        endTime: data.endTime,
+        script: data.script,
+        id: data.id
       }
       segmentStore.importScript(segment)
     }
@@ -58,6 +83,20 @@ export default class BaseScript extends Vue {
       index,
       text
     })
+  }
+
+  async saveScript() {
+    const segments = segmentStore.segments
+    for (let i = 0; i < segments.length; i++) {
+      await DataStore.save(
+        new Segment({
+          speaker: segments[i].speaker,
+          startTime: segments[i].startTime,
+          endTime: segments[i].endTime,
+          script: segments[i].script
+        })
+      )
+    }
   }
 
   @Prop() currentTime!: number
