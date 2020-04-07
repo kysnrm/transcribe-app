d3447b55f801ca9c91145455810f99606885fc94
kysnrm <kysn.rm@gmail.com>
2020-04-07 11:28:57 +0900

    S3 への音声のアップロードを可能に

---
+++ components/BaseUploader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="file" accept="audio/mp3" @change="selectFile" />
+    <button @click="uploadAudio">音声をアップロード</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+import Amplify, { Storage } from 'aws-amplify'
+import awsmobile from '@/src/aws-exports'
+Amplify.configure(awsmobile)
+
+@Component
+export default class BaseUploader extends Vue {
+  uploadFile: string = ''
+  selectFile(e: Event) {
+    this.uploadFile = e.target.files[0]
+  }
+
+  uploadAudio() {
+    Storage.put('test.mp3', this.uploadFile)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err))
+  }
+}
+</script>
